b5ffdb5b62cbc119c51afcc0ddc746e876d2554c
Jack Weller <github@jackweller.me>
2025-07-23 18:11:51 +1000

    hide things that i dont want to be seen yet

--- src/components/blog/index.jsx
+++ src/components/blog/index.jsx
@@ -5,30 +5,27 @@ import moment from "moment";
 import content from "../../../content.json";
 
 export function BlogComponent() {
-    function calculateReadTime(text) {
-        const wordCount = text.split(/\s+/).length;
-        const wordsPerMinute = 300;
-        return Math.ceil(wordCount / wordsPerMinute);
-    }
 
     return (
         <section id="blog">
             <h2>Blog</h2>
             <div id="blogItems" className={styles["items"]}>
-                {content.blog.map((blog, index) => (
-                    <div key={index} className={styles["item"]}>
-                        <a href={`/blog/${blog.id}/`}>
-                            <img src={blog.image} />
-                        </a>
-                        <h3>{blog.title}</h3>
-                        <p>
-                            <span>
-                                {moment(blog.date, "DD/MM/YYYY").fromNow()}
-                            </span>
-                            <span>{}</span>
-                        </p>
-                    </div>
-                ))}
+                {content.blog.map((blog, index) => {
+                    if (blog.hide) return null;
+                    return (
+                        <div key={index} className={styles["item"]}>
+                            <a href={`/blog/${blog.id}/`}>
+                                <img src={blog.image} />
+                            </a>
+                            <h3>{blog.title}</h3>
+                            <p>
+                                <span>
+                                    {moment(blog.date, "DD/MM/YYYY").fromNow()}
+                                </span>
+                            </p>
+                        </div>
+                    );
+                })}
             </div>
         </section>
     );
