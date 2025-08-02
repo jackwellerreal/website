7cc02eec50f80cc57c206f5ed722360fa5e29e4f
Jack Weller <github@jackweller.me>
2025-08-02 12:23:57 +1000

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./styles.css";
 
 export const metadata = {
